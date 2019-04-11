a7537afd703e081524633874f51f135d71615b39
crvicek <crvicek@yahoo.com>
2019-04-11 18:48:04 +0200

    bugfix

--- src/api/apiCheck.ts
+++ src/api/apiCheck.ts
@@ -1,5 +1,4 @@
 import AutoMatch from "./entity";
-import { idee } from '../../idea'
 import Idea from "../ideas/entity";
 
 const request = require('superagent');
@@ -11,7 +10,7 @@ export default async function apiCheck(object) {
   //   "requested-hits": "10",
   //   "view": "bibliographic,passage" // important 
   // }
-  console.log('MY SNASWEEERRRR!!!', idee.filter(ans => ans.answers), idea)
+  // console.log('MY SNASWEEERRRR!!!', idee.filter(ans => ans.answers), idea)
   const entry = new AutoMatch
   request
     .get('https://39631edd-e73e-426e-8ee5-524a923295b1.mock.pstmn.io/v2.1/search?ticket=495fdecd03dbec28ed86fa8cb40758ae')
