0e77828f70fa57880599d2dba865f94a0567121d
crvicek <crvicek@yahoo.com>
2019-04-14 00:53:58 +0200

    Automatch Saves Ticket & ATM JSON

--- src/api/apiCheck.ts
+++ src/api/apiCheck.ts
@@ -2,10 +2,14 @@ import AutoMatch from "./entity";
 import Idea from "../ideas/entity";
 import { NotFoundError } from "routing-controllers";
 import User from "../users/entity";
+require('dotenv').config()
 
 
 const request = require('superagent');
 const atmkey = process.env.AUTOMATCH_AUTH
+console.log(atmkey)
+
+// let myTicket;
 
 export default async function apiCheck(object) {
   // Find an idea
@@ -31,33 +35,51 @@ export default async function apiCheck(object) {
   const json = {
     "query": "Technology which enables a trackpad or touchscreen to recognize more than one or more than two points of contact with the display.",
     "requested-hits": "10",
-    "view": "bibliographic,passage" 
+    "view": "bibliographic,passage"
   }
-  // console.log('MY JSOOOOONNN!!!', json)
+  console.log('MY JSOOOOONNN!!!', json)
   // console.log('MY USR.ID!!!', idd)
   // console.log('MY USR!!!', usr.id)
   request
+    .get('https://www.google.com')
     // .get('https://api.auto-match.se/v2.1/index')
-    .post('https://api.auto-match.se/v2.1/search')
-    .set('Authorization', atmkey)
-
-    .set('reference-number', usr.id)
-    .set('Content-Type', 'application/json')
-    .send(json)
+    // .post('https://api.auto-match.se/v2.1/search')
+    // .set('Authorization', atmkey)
+    // .set('reference-number', usr.id)
+    // .set('Content-Type', 'application/json')
+    // .send(json)
     .then(response => {
-      entry.autoMatch = response.body.data
-      console.log('MY RESPONSEEE!!', response.body.data)
+      console.log('GoogleRESPONSEbody??', response.body)
+      entry.ticket = "158956472643eaf9bf994a9bf49f4838" // = response.body.data
+      // myTicket = "158956472643eaf9bf994a9bf49f4838" // = response.body.data
       return entry.save()
-    }
-      // entry.autoMatch = response.body.data['automatch-results']['index-1']
-    )
-    .then(rsp => {
-      const update = { autoMatch: rsp }
-      console.log('RESPONSE FOR IDEA', rsp)
-      return Idea.merge(idea, update).save()
-
     })
+    .then(atm => getResults(atm))
+    // .then(rsp => {
+    //   const update = { autoMatch: rsp }
+    //   return Idea.merge(idea, update).save()
+    // })
     .catch(error => console.log(error))
+}
 
+async function getResults(atm) {
+  setTimeout(
+    () => {
+      console.log('MYTICKEEETTT IN DELAAAYY', atm.ticket)
+      request
+        .get('https://www.google.com')
 
+        // .get(`https://api.auto-match.se/v2.1/search?ticket=${atm.ticket}`)
+        // .set('Authorization', atmkey)
+        .then(response => {
+          const patents = { autoMatch: response.body } // response.body.data['automatch-results']['index-1'] }
+          return AutoMatch.merge(atm, patents).save()
+          // entry.autoMatch = response.body.data['automatch-results']['index-1']
+          // console.log('MY MASSIVE RESPONSE', response.body.data['automatch-results']['index-1'])
+        })
+        .catch(error => console.log(error))
+
+    },
+    1000 * 5 // Wait 1 minute for automatch to calculate results
+  )
 }
--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import UploadController from "./files/controller";
 import QuestController from "./quest/controller";
 import AutoMatchController from "./api/controller";
 require('dotenv').config()
-
+console.log('DOT___ENV___PORT!!!',process.env.PORT)
 
 const port = process.env.PORT || 4000
 
