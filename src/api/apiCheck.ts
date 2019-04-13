58b4f0241926f5c9eaa97db447396f0946d2288a
crvicek <crvicek@yahoo.com>
2019-04-13 17:52:48 +0200

    fix automatch?

--- src/api/apiCheck.ts
+++ src/api/apiCheck.ts
@@ -1,7 +1,7 @@
 import AutoMatch from "./entity";
-import Idea from "../ideas/entity";
-import { NotFoundError } from "routing-controllers";
-import User from "../users/entity";
+// import Idea from "../ideas/entity";
+// import { NotFoundError } from "routing-controllers";
+// import User from "../users/entity";
 
 
 const request = require('superagent');
@@ -9,31 +9,32 @@ const atmkey = process.env.AUTOMATCH_AUTH
 
 export default async function apiCheck(object) {
   // Find an idea
-  let idea = await Idea.findOne(object.id).catch(err => console.log(err))
-  if (!idea) throw new NotFoundError('Cannot find idea')
+  // let idea = await Idea.findOne(object.id).catch(err => console.log(err))
+  // if (!idea) throw new NotFoundError('Cannot find idea')
 
-  console.log('MY IDEA!!!', idea.user)
+  // console.log('MY IDEA!!!', idea.user)
   // Find the user
-  const usr = await User.findOne({ where: idea.user }).catch(err => console.log(err))
-  if (!usr) throw new NotFoundError('Cannot find user')
+  // const usr = await User.findOne({ where: idea.user }).catch(err => console.log(err))
+  // if (!usr) throw new NotFoundError('Cannot find user')
 
   // Extract relevant text from the idea
-  let idd = idea.idea[4].answers[0].qAnswer
+  // let idd = idea.idea[4].answers[0].qAnswer
 
   // Create new Automatch assign idea and user to it
   const entry = new AutoMatch
-  entry.idea = object.id
-  entry.user = usr
+  // entry.idea = object.id
+  // entry.user = usr
 
   // Prepare JSON for AutoMatch
   const json = {
-    "query": idd,
+    "query": "Technology which enables a trackpad or touchscreen to recognize more than one or more than two points of contact with the display.",// idd,
     "requested-hits": "10",
     "view": "bibliographic,passage" // important 
   }
   console.log('MY JSOOOOONNN!!!', json)
-  console.log('MY USR.ID!!!', usr.id)
-  console.log('MY USR!!!', usr)
+  // console.log('MY USR.ID!!!', usr.id)
+  // console.log('MY USR!!!', usr)
+  console.log('MY OBJECT!!!', object)
   request
     .get('https://api.auto-match.se/v2.1/index')
     // .post('https://api.auto-match.se/v2.1/search')
@@ -51,7 +52,7 @@ export default async function apiCheck(object) {
     .then(rsp => console.log('Response from saving', rsp))
     .catch(error => console.log(error))
 
-  const update = { autoMatch: entry }
-  await Idea.merge(idea, update).save()
+  // const update = { autoMatch: entry }
+  // await Idea.merge(idea, update).save()
 
 }
