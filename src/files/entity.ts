1045a889f0158866f9da3fd0f92a1f58b480075d
crvicek <crvicek@yahoo.com>
2019-04-18 15:20:38 +0200

    Bug fix

--- src/files/entity.ts
+++ src/files/entity.ts
@@ -1,21 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, CreateDateColumn, Column, ManyToOne } from 'typeorm'
-import { BaseEntity } from 'typeorm/repository/BaseEntity'
-import User from '../users/entity';
+// import { Entity, PrimaryGeneratedColumn, CreateDateColumn, Column, ManyToOne } from 'typeorm'
+// import { BaseEntity } from 'typeorm/repository/BaseEntity'
+// import User from '../users/entity';
 
 
-@Entity()
-export default class Upload extends BaseEntity {
+// @Entity()
+// export default class Upload extends BaseEntity {
 
-  @PrimaryGeneratedColumn()
-  id?: number
+//   @PrimaryGeneratedColumn()
+//   id?: number
 
-  @Column('text', { nullable: true })
-  path: string
+//   @Column('text', { nullable: true })
+//   path: string
 
-  @ManyToOne(_type => User, user => user.uploads)
-  user: User;
+//   @ManyToOne(_type => User, user => user.uploads)
+//   user: User;
 
-  @CreateDateColumn({ type: "timestamp" })
-  createdAt: Date;
+//   @CreateDateColumn({ type: "timestamp" })
+//   createdAt: Date;
 
-}
+// }
